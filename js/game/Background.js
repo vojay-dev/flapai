f83bed7122ff4c1392920beff3276acd0e542d70
Volker Janz <volker.janz@innogames.com>
2019-04-30 12:29:57 +0200

    Fix scrolling background offset issue (thx @BattleRattle)

--- js/game/Background.js
+++ js/game/Background.js
@@ -2,27 +2,19 @@ class Background {
 
   constructor(img, speed = 1) {
     this.img = img;
-
-    this.x1 = 0;
-    this.x2 = width;
-
     this.speed = speed;
+
+    this.x = width;
   }
 
   update() {
-    image(this.img, 0, 0, width, height);
-    image(this.img, this.x1, 0, width, height);
-    image(this.img, this.x2, 0, width, height);
+    image(this.img, this.x - width, 0, width, height);
+    image(this.img, this.x, 0, width, height);
     
-    this.x1 -= this.speed;
-    this.x2 -= this.speed;
-    
-    if (this.x1 < -width) {
-      this.x1 = width;
-    }
-  
-    if (this.x2 < -width) {
-      this.x2 = width;
+    this.x -= this.speed;
+
+    if (this.x < 0) {
+      this.x = width;
     }
   }
 
